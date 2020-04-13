d459bd798126ff006ceaf02bc1eb100b4604c5ad
Jay <Jayraj@ash-software.com>
2020-04-13 19:00:57 +0100

    added home page with fixed navigation menu

--- src/App.js
+++ src/App.js
@@ -6,12 +6,19 @@ import Home from './components/Home';
 import Login from './components/Login';
 import Error from './components/Error';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import CowImage from './content/images/cow-feed.jpg';
+
+var backgrounImage = {
+  width: "100%",
+  height: "100%",
+  backgroundImage: `url(${CowImage})`  
+};
 
 function App() {
   return (   
 
       <BrowserRouter >
-      <div> 
+      <div className="MHome"> 
         <Navbar />
         <Switch>
       <Route path="/" component={Login} exact />
---
+++ src/components/NavMenu/NavMenu.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavbarToggler, MDBCollapse, MDBNavItem, MDBNavLink, MDBContainer, MDBMask, MDBView } from 'mdbreact';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+class NavMenu extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      collapse: false,
+      isWideEnough: false,
+    };
+    this.onClick = this.onClick.bind(this);
+  }
+
+  onClick() {
+    this.setState({
+      collapse: !this.state.collapse,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <header>
+          <Router>
+            <MDBNavbar color="bg-primary" fixed="top" dark expand="md" scrolling transparent>
+              <MDBNavbarBrand href="/">
+                <strong>Navbar</strong>
+              </MDBNavbarBrand>
+              {!this.state.isWideEnough && <MDBNavbarToggler onClick={this.onClick} />}
+              <MDBCollapse isOpen={this.state.collapse} navbar>
+                <MDBNavbarNav left>
+                  <MDBNavItem active>
+                    <MDBNavLink to="/home">Home</MDBNavLink>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                    <MDBNavLink to="/">Login</MDBNavLink>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                    <MDBNavLink to="#">Profile</MDBNavLink>
+                  </MDBNavItem>
+                </MDBNavbarNav>
+              </MDBCollapse>
+            </MDBNavbar>
+          </Router>          
+        </header>      
+      </div>
+    );
+  }
+}
+
+export default NavMenu;
