17721f38e631c7cdf88b587e97c7accd80de8c15
Jay <Jayraj@ash-software.com>
2020-04-13 13:54:48 +0100

    added router to the project with error route

---
+++ src/components/Error/Error.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Error.css';
+
+export default function Error() {
+  return(
+  <div>
+    Path not exists !!!< br />
+       Path not exists !!!< br />
+      Path not exists !!!< br /> 
+      Path not exists !!!< br />
+      Path not exists !!!< br />
+     </div>
+  );
+};
+
+//export default Error;
